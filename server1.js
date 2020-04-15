ddf77a6a17610d0525e6b028d96ca98675e5f47e
unknown <tx17079@bristol.ac.uk>
2020-04-15 16:15:11 +0100

    added api revives call

--- server1.js
+++ server1.js
@@ -3,7 +3,19 @@ let HTTP = require('http');
 let FS = require("fs");
 let mime = require("mime-types");
 let mysql = require("mysql");
-start(8080);
+let fetch = require("node-fetch");
+let yaml = require('js-yaml');
+let fs   = require('fs');
+const WebSocket = require('ws');
+
+
+var con;
+
+var serviceID = "s:jtwebtech"
+
+start_server(8080);
+connect_db(read_yaml());
+request_exp();
 
 var pageMap = {
   "/":"index.html",
@@ -15,13 +27,96 @@ var pageMap = {
 }
 
 // Provide a service to localhost only.
-function start(port) {
+function start_server(port) {
   let service = HTTP.createServer(handle);
   service.listen(port, 'localhost');
 }
 
+function connect_db(doc){
+  con = mysql.createConnection({
+    host: doc.host,
+    user: doc.user,
+    password: doc.password
+  });
+  con.connect(function(err) {
+    if (err) throw err;
+    console.log("Connected!");
+  });
+  var sel = "USE webtech;";
+  con.query(sel, function (err, result) {
+    if (err) throw err;
+  })
+}
+
+function request_exp() {
+  let socket = new WebSocket("wss://push.planetside2.com/streaming?environment=ps2&service-id=" + serviceID);
+  socket.onmessage = handle_exp_response;
+  socket.onopen = () => start(socket);
+  
+}
+
+function handle_exp_response(event) {
+  var jsonData = JSON.parse(event.data);
+  // console.log(jsonData);
+  //ignore heartbeat messages
+  if (jsonData.type == "heartbeat") {
+    return
+  }
+  else if (jsonData.type == "serviceMessage") {
+    con.query("SELECT COUNT(id) FROM characters WHERE id =" + jsonData.payload.character_id , function (err, result) {
+      if (err) throw err;
+      console.log(result[0]["COUNT(id)"])
+      if (result[0]["COUNT(id)"]==0) {
+        console.log("JS sucks")
+        create_character(jsonData.payload.character_id);
+      }
+    })
+    var sql = "UPDATE characters SET resurrections = IFNULL(resurrections, 0) + 1 WHERE id = "+ jsonData.payload.character_id +";";
+    con.query(sql, function (err, result) {
+      if (err) throw err;
+    })
+  }
+}
+
+//duplicate safe function to create a character entry from their id
+function create_character(id){
+  //request username
+  var requestString = "http://census.daybreakgames.com/"+ serviceID +"/get/ps2:v2/character/?character_id=" + id + "&c:show=name";
+  fetch(requestString).then( function (result) {
+    result.json().then(function (jsonData) {
+      console.log(jsonData)
+      var sql = "INSERT INTO characters(id, username) VALUES (" + id + ",'" + jsonData.character_list[0].name.first +"') ON DUPLICATE KEY UPDATE id=id;";
+      con.query(sql, function (err, result) {
+        if (err) throw err;
+      })
+    })
+  });
+}
+
+function start(socket) {
+  socket.send(JSON.stringify({ "service": "event", "action": "subscribe", "characters": ["all"], "eventNames": ["GainExperience_experience_id_7"] }));
+}
+
+function read_yaml() {
+  try {
+    var doc = yaml.safeLoad(fs.readFileSync('properties.yaml', 'utf8'));
+  } catch (e) {
+    console.log(e);
+  }
+  return doc
+}
+
+function callapi(request,response){
+  var i=request.url.indexOf("api")+4
+  fetch("http://census.daybreakgames.com/s:jtwebtech/" + request.url.slice(i)).then(handle);
+  function handle(apiresponse) {
+    apiresponse.text().then( (text) => reply(response, text, mime.contentType(".json")));  
+  }
+}
+
 // Deal with a request.
 function handle(request, response) {
+  var params = parseparameters(request.url);
   console.log("Method:", request.method);
   console.log("URL:", request.url);
   //console.log("Headers:", request.headers);
@@ -38,7 +133,47 @@ function handle(request, response) {
       sendFile(file, response, type);
     }
   }
+  else if (request.url.startsWith("/api/")){
+    callapi(request,response);
+  }
   console.log();
+  console.log(params)
+  if ("res" in params){
+    console.log("print")
+    var thing = "SELECT username FROM characters ORDER BY resurrections DESC LIMIT 10;";
+    con.query(thing, function (err, result) {
+      var content = result.map((x) => x.username).join()
+      reply(response, content, "text/plain")
+      if (err) throw err;
+    });
+  }
+}
+
+function isnull(a,b){
+  if (a==null){
+    return b;
+  }
+  return a;
+}
+
+function parseparameters(url){
+  console.log(url)
+  var dict = {};
+  if (url.includes("?")){
+    var temp = url.split("?").pop();
+    console.log(temp)
+    let params = temp.split("&");  
+    console.log(params)
+    for (var i in params){
+      var p = params[i];
+      console.log(typeof(p));
+      var name = p.substring(0,p.indexOf("="));
+      var value = p.substring(p.indexOf("=")+1, p.length);
+      dict[name] = value;
+    }
+  }
+  console.log(dict)
+  return dict;
 }
 
 async function sendPage(filePath, response) {
@@ -49,7 +184,7 @@ async function sendPage(filePath, response) {
     templateMap["time"] = time;
   }
   content = template(content, templateMap);
-  console.log("Content: " + content);
+  // console.log("Content: " + content);
   reply(response, content, 'text/html');
 }
 
@@ -82,7 +217,7 @@ async function sendFile(filePath, response, mimeType) {
 // Send a reply.
 function reply(response, content, mimeType) {
   let hdrs = { 'Content-Type': mimeType };
-  response.writeHead(200, hdrs);  // 200 = OK
+  response.writeHead(200, hdrs);  // 200 = SNAZZY
   response.write(content);
   response.end();
 }
