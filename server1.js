8b742c7be4c207be6abb0cd22bebe804758cb336
TobleroneSwordfish <jonathan.hall859@gmail.com>
2020-04-16 16:11:31 +0100

    reeeeeeeeefactor

--- server1.js
+++ server1.js
@@ -32,6 +32,15 @@ function start_server(port) {
   service.listen(port, 'localhost');
 }
 
+function read_yaml() {
+  try {
+    var doc = yaml.safeLoad(fs.readFileSync('properties.yaml', 'utf8'));
+  } catch (e) {
+    console.log(e);
+  }
+  return doc
+}
+
 function connect_db(doc){
   con = mysql.createConnection({
     host: doc.host,
@@ -51,27 +60,34 @@ function connect_db(doc){
 function request_exp() {
   let socket = new WebSocket("wss://push.planetside2.com/streaming?environment=ps2&service-id=" + serviceID);
   socket.onmessage = handle_exp_response;
-  socket.onopen = () => start(socket);
-  
+  socket.onopen = () => subscribe_exp(socket);  
+}
+
+function subscribe_exp(socket) {
+  socket.send(JSON.stringify({ "service": "event", "action": "subscribe", "characters": ["all"], "eventNames": ["GainExperience_experience_id_7"] }));
 }
 
 function handle_exp_response(event) {
   var jsonData = JSON.parse(event.data);
-  // console.log(jsonData);
+
   //ignore heartbeat messages
   if (jsonData.type == "heartbeat") {
     return
   }
   else if (jsonData.type == "serviceMessage") {
-    con.query("SELECT COUNT(id) FROM characters WHERE id =" + jsonData.payload.character_id , function (err, result) {
+    var payload = jsonData.payload;
+    //count all entries with the incoming ID
+    con.query("SELECT COUNT(id) FROM characters WHERE id =" + payload.character_id , function (err, result) {
       if (err) throw err;
-      console.log(result[0]["COUNT(id)"])
+      //console.log(result[0]["COUNT(id)"])
+      //result is a array of objects, so this ensues
       if (result[0]["COUNT(id)"]==0) {
-        console.log("JS sucks")
-        create_character(jsonData.payload.character_id);
+        //character does not exist so we create it
+        create_character(payload.character_id);
       }
     })
-    var sql = "UPDATE characters SET resurrections = IFNULL(resurrections, 0) + 1 WHERE id = "+ jsonData.payload.character_id +";";
+    //actually increase the resurrections count
+    var sql = "UPDATE characters SET resurrections = IFNULL(resurrections, 0) + 1 WHERE id = "+ payload.character_id +";";
     con.query(sql, function (err, result) {
       if (err) throw err;
     })
@@ -85,6 +101,7 @@ function create_character(id){
   fetch(requestString).then( function (result) {
     result.json().then(function (jsonData) {
       console.log(jsonData)
+      //for some reason this is the best way to do duplicate safe insertion
       var sql = "INSERT INTO characters(id, username) VALUES (" + id + ",'" + jsonData.character_list[0].name.first +"') ON DUPLICATE KEY UPDATE id=id;";
       con.query(sql, function (err, result) {
         if (err) throw err;
@@ -93,20 +110,7 @@ function create_character(id){
   });
 }
 
-function start(socket) {
-  socket.send(JSON.stringify({ "service": "event", "action": "subscribe", "characters": ["all"], "eventNames": ["GainExperience_experience_id_7"] }));
-}
-
-function read_yaml() {
-  try {
-    var doc = yaml.safeLoad(fs.readFileSync('properties.yaml', 'utf8'));
-  } catch (e) {
-    console.log(e);
-  }
-  return doc
-}
-
-function callapi(request,response){
+function call_api(request,response){
   var i=request.url.indexOf("api")+4
   fetch("http://census.daybreakgames.com/s:jtwebtech/" + request.url.slice(i)).then(handle);
   function handle(apiresponse) {
@@ -114,41 +118,49 @@ function callapi(request,response){
   }
 }
 
-// Deal with a request.
+//main HTTP handle function
 function handle(request, response) {
-  var params = parseparameters(request.url);
+  var params = parse_parameters(request.url);
+  
   console.log("Method:", request.method);
   console.log("URL:", request.url);
-  //console.log("Headers:", request.headers);
+  console.log("Params: ", params)
 
+  //check if the requested URL maps to a file
   var file = pageMap[request.url];
-  console.log("Filename found: " + file);
   if (file) {
+    console.log("Filename found: " + file);
     var type = mime.contentType(file);
     console.log("Content-Type: " + type);
+    //if it's an HTML page, send it off for templating
     if (type.includes("text/html")) {
-      sendPage(file, response);
+      send_page(file, response);
     }
-    else {
-      sendFile(file, response, type);
+    else { //otherwise just send the file
+      send_file(file, response, type);
     }
   }
+  //reflect request on to the daybreak API
   else if (request.url.startsWith("/api/")){
-    callapi(request,response);
+    call_api(request,response);
   }
+
   console.log();
-  console.log(params)
+  //client has requested the 10 characters with the most resurrections
   if ("res" in params){
-    console.log("print")
-    var thing = "SELECT username FROM characters ORDER BY resurrections DESC LIMIT 10;";
+    var thing = "SELECT username, resurrections FROM characters ORDER BY resurrections DESC LIMIT 10;";
     con.query(thing, function (err, result) {
-      var content = result.map((x) => x.username).join()
+      //build a string from the SQL result array
+      var content = result.map((x) => "(" + x.username + "," + x.resurrections + ")").join(", ")
       reply(response, content, "text/plain")
       if (err) throw err;
     });
   }
 }
 
+
+//we may use this one day
+//C# has this as an operator nehhh
 function isnull(a,b){
   if (a==null){
     return b;
@@ -156,7 +168,8 @@ function isnull(a,b){
   return a;
 }
 
-function parseparameters(url){
+//returns a dict mapping parameter names to values
+function parse_parameters(url){
   console.log(url)
   var dict = {};
   if (url.includes("?")){
@@ -176,9 +189,11 @@ function parseparameters(url){
   return dict;
 }
 
-async function sendPage(filePath, response) {
+//template and send an HTML page
+async function send_page(filePath, response) {
   var content = await FS.readFileSync("./" + filePath, "utf8");
   var templateMap = {};
+  //here we add stuff to the template map to be sent to the client
   if (filePath == "index.html") {
     var time = (new Date()).toDateString();
     templateMap["time"] = time;
@@ -208,7 +223,8 @@ function template(content, templateMap) {
   return content;
 }
 
-async function sendFile(filePath, response, mimeType) {
+//just read and send a file normally
+async function send_file(filePath, response, mimeType) {
   var content = await FS.readFileSync("./" + filePath, "utf8");
   reply(response, content, mimeType);
 }
